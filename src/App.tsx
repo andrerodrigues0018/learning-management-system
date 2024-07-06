5079efe4093e7972c9226105d56bc18e8f0ff78f
André Rodrigues <andre.rodrigues0018@gmail.com>
2024-07-05 23:21:32 -0300

    feat: starting theme with radix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 
+import { Theme,  Flex, Text, Button, ThemePanel   } from '@radix-ui/themes';
+
 function App() {
   
 
   return (
     <>
-      <div>
-        Olá mundo!
-      </div>
+      <Theme accentColor="crimson" grayColor="sand" radius="large" scaling="105%">
+        <Flex direction="row" gap="2">
+          <Text>Hello from Radix Themes :)</Text>
+          <Button>Let's go</Button>
+        </Flex>
+        <ThemePanel></ThemePanel>
+      </Theme>
       
     </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import App from './App.tsx'
-import './index.css'
+// import './index.css'
+import '@radix-ui/themes/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
