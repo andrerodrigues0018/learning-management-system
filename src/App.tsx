945f51e49733d00e2ae4cf1766dc7725d06e2d0c
André Rodrigues <andre.rodrigues0018@gmail.com>
2024-07-07 23:50:56 -0300

    feat: implement tab system

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,52 @@
 
-import { Theme,  Flex, Text, Button, ThemePanel   } from '@radix-ui/themes';
+import { Theme, Container } from '@radix-ui/themes';
+import * as Tabs from '@radix-ui/react-tabs';
+
+import './App.css';
+
 
 function App() {
-  
+
 
   return (
     <>
-      <Theme accentColor="crimson" grayColor="sand" radius="large" scaling="105%">
-        <Flex direction="row" gap="2">
-          <Text>Hello from Radix Themes :)</Text>
-          <Button>Let's go</Button>
-        </Flex>
-        <ThemePanel></ThemePanel>
+      <Theme accentColor="green" appearance='dark' grayColor="sand" radius="large">
+        <Container className='main-container'>
+          <Tabs.Root className="TabsRoot" defaultValue="tab-student">
+            
+              <Tabs.List className="TabsList" >
+                <Tabs.Trigger className="TabsTrigger" value="tab-student">
+                  Student
+                </Tabs.Trigger>
+                <Tabs.Trigger className="TabsTrigger" value="tab-adm">
+                  Adm
+                </Tabs.Trigger>
+              </Tabs.List>
+
+
+            <Tabs.Content className="TabsContent" value="tab-student">
+              <p className="Text">Visão do estudante</p>
+
+              <div style={{ display: 'flex', marginTop: 20, justifyContent: 'flex-end' }}>
+                <button className="Button green">Save changes</button>
+              </div>
+            </Tabs.Content>
+
+            <Tabs.Content className="TabsContent" value="tab-adm">
+              <p className="Text">Change your password here. After saving, you'll be logged out.</p>
+
+
+
+              <div style={{ display: 'flex', marginTop: 20, justifyContent: 'flex-end' }}>
+                <button className="Button green">Change password</button>
+              </div>
+            </Tabs.Content>
+          </Tabs.Root>
+        </Container>
+
+        {/* <ThemePanel></ThemePanel> */}
       </Theme>
-      
+
     </>
   )
 }
