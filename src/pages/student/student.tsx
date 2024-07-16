cbd5f6d26b2a1641c55e5d1ce543e3407da5aea2
André Rodrigues <andre.rodrigues0018@gmail.com>
2024-07-15 23:11:47 -0300

    feat: apply multiple layouts

--- src/pages/student/student.tsx
+++ src/pages/student/student.tsx
@@ -1,51 +1,56 @@
-
-
+import React, { useState } from 'react';
 import { Flex, Text, Radio } from "@radix-ui/themes";
-import DropdownMenuDemo from "../../components/DropdownMenuDemo"
-import { Player } from "../../components/player"
+import DropdownMenuDemo from "../../components/DropdownMenuDemo";
+import { Player } from "../../components/player";
 import './student-style.css';
 
+interface LayoutOptions {
+  value: string;
+  label: string;
+}
+
+const layoutOptions: LayoutOptions[] = [
+  { value: 'Default', label: 'Default' },
+  { value: 'Comfortable', label: 'Comfortable' },
+  { value: 'Compact', label: 'Compact' },
+];
 
 function Student() {
+  const [selectedLayout, setSelectedLayout] = useState<string>(layoutOptions[0].value); // Initial state
 
+  const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedLayout(event.target.value);
+  };
 
   return (
     <>
       <DropdownMenuDemo />
       <p>a</p>
       <Flex align="start" direction="row" gap="5">
-        <Flex asChild gap="2">
-          <Text as="label" size="2">
-            <Radio name="example" value="1" defaultChecked />
-            Default
-          </Text>
-        </Flex>
-
-        <Flex asChild gap="2">
-          <Text as="label" size="2">
-            <Radio name="example" value="2" />
-            Comfortable
-          </Text>
-        </Flex>
-
-        <Flex asChild gap="2">
-          <Text as="label" size="2">
-            <Radio name="example" value="3" />
-            Compact
-          </Text>
-        </Flex>
+        {layoutOptions.map((option) => (
+          <Flex asChild gap="2" key={option.value}>
+            <Text as="label" size="2">
+              <Radio
+                name="example"
+                value={option.value}
+                checked={selectedLayout === option.value}
+                onChange={handleRadioChange}
+              />
+              {option.label}
+            </Text>
+          </Flex>
+        ))}
       </Flex>
-      <Flex id="elements">
-      
+      <Flex id="elements" className={`${selectedLayout}`}>
         <div id="player-layout" className="element">
-          <Player/>
+          <Player />
         </div>
         <div id="iframe-layout" className="element">
           <iframe src="https://stackblitz.com/edit/vidstack-examples-sjm3aw?embed=1&file=README.md"></iframe>
         </div>
       </Flex>
     </>
-  )
+  );
 }
 
-export default Student
+export default Student;
