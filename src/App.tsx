6d449cc2d4d675ba05986c9f9302fe15e1d5eaa4
André Rodrigues <andre.rodrigues0018@gmail.com>
2024-07-16 23:25:17 -0300

    feat: implemented first genially educational tool

--- src/App.tsx
+++ src/App.tsx
@@ -11,32 +11,27 @@ function App() {
   return (
     <>
       <Theme accentColor="green" appearance='dark' grayColor="sand" radius="large">
-          <Tabs.Root className="TabsRoot" defaultValue="tab-student">
-            
-              <Tabs.List className="TabsList" >
-                <Tabs.Trigger className="TabsTrigger" value="tab-student">
-                  Student
-                </Tabs.Trigger>
-                <Tabs.Trigger className="TabsTrigger" value="tab-adm">
-                  Adm
-                </Tabs.Trigger>
-              </Tabs.List>
+        <Tabs.Root className="TabsRoot" defaultValue="tab-student">
+
+          <Tabs.List className="TabsList" >
+            <Tabs.Trigger className="TabsTrigger" value="tab-student">
+              Student
+            </Tabs.Trigger>
+            <Tabs.Trigger className="TabsTrigger" value="tab-adm">
+              Adm
+            </Tabs.Trigger>
+          </Tabs.List>
 
 
-            <Tabs.Content className="TabsContent" value="tab-student">
-              <Student/>
-            </Tabs.Content>
+          <Tabs.Content className="TabsContent" value="tab-student">
+            <Student />
+          </Tabs.Content>
 
-            <Tabs.Content className="TabsContent" value="tab-adm">
-              <p className="Text">Change your password here. After saving, you'll be logged out.</p>
-
-
-
-              <div style={{ display: 'flex', marginTop: 20, justifyContent: 'flex-end' }}>
-                <button className="Button green">Change password</button>
-              </div>
-            </Tabs.Content>
-          </Tabs.Root>
+          <Tabs.Content className="TabsContent" value="tab-adm">
+                <iframe title="Fill in the blanks game" frameborder="0" width="1200" height="675"  src="https://view.genially.com/669726733037eb3b9d401fd1" type="text/html" allowscriptaccess="always" allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe> 
+             
+          </Tabs.Content>
+        </Tabs.Root>
 
         {/* <ThemePanel></ThemePanel> */}
       </Theme>
