809a4eb538ca817bf21d6bdce001de7e7473186a
André Rodrigues <andre.rodrigues0018@gmail.com>
2024-07-23 23:37:57 -0300

    feat: centralize iframe sections

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -1,7 +1,7 @@
 export function Game() {
     return (
         <>
-            <iframe title="Fill in the blanks game" frameborder="0" style={ { 'width': '100%', height: '100%' } }  src="https://view.genially.com/669726733037eb3b9d401fd1" type="text/html" allowscriptaccess="always" allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe> 
+            <iframe title="Fill in the blanks game" style={ { 'width': '100%', height: '100%' } }  src="https://view.genially.com/669726733037eb3b9d401fd1" type="text/html" allowscriptaccess="always" allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe> 
 
         </>
     );
