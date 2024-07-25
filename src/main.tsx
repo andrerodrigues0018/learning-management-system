b3759bd2fc790a592f1bc546fb04fdbb4f6a75df
André Rodrigues <35631672+andrerodrigues0018@users.noreply.github.com>
2024-07-25 02:14:26 +0000

    fix: remove unused css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.tsx'
-// import './index.css'
+
 import '@radix-ui/themes/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
