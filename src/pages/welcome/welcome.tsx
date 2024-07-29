8e6b39affd45a9d558fe333dc95c92b54be70be0
André Rodrigues <andre.rodrigues0018@gmail.com>
2024-07-29 00:10:06 -0300

    feat: import radix dark theme

--- src/pages/welcome/welcome.tsx
+++ src/pages/welcome/welcome.tsx
@@ -1,9 +1,22 @@
+import { Theme, Flex, Text } from '@radix-ui/themes';
+
 function Welcome() {
 
 
     return (
         <>
-        olá mundo
+            <Theme accentColor="green" appearance='dark' grayColor="sand" radius="large">
+                <Flex className='main-container'>
+                    <Flex className='social-proof'>
+                        <Text>Social Proof</Text>
+
+                    </Flex>
+                    <Flex className='right-side'>
+                        <Text>Welcome to platform</Text>
+                        
+                    </Flex>
+                </Flex>
+            </Theme>
         </>
     )
 }
