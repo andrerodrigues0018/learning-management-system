2d02734bcf89f0cc16aeed6ea1c77a3b93726839
André Rodrigues <andre.rodrigues0018@gmail.com>
2024-07-30 23:15:48 -0300

    feat: iniate menu component with accordion radix componente

--- src/pages/course/course.tsx
+++ src/pages/course/course.tsx
@@ -1,12 +1,39 @@
 import { Theme, Flex, Text, Button } from '@radix-ui/themes';
-// import './course.css'
+import * as Accordion from '@radix-ui/react-accordion';
+import './course.css'
 import codeschool from '../../assets/codeschool.svg';
-import { CheckIcon, RocketIcon, MixIcon } from '@radix-ui/react-icons';
+import { CheckIcon, RocketIcon, MixIcon, ChevronDownIcon } from '@radix-ui/react-icons';
+
 
 function Course() {
     return (
         <>
-            <p>cursos</p>
+            <Theme accentColor="blue" appearance='dark' grayColor="sand" radius="large">
+
+                <Flex id='side-menu'>
+                    <img id="menu-logo" />
+                    <Accordion.Root type="single" collapsible>
+                        <Accordion.Item value="item-1">
+                            <Accordion.Header>
+                                <Accordion.Trigger className="AccordionTrigger">
+                                    <span>Dashboard</span>
+                                </Accordion.Trigger>
+                            </Accordion.Header>
+                        </Accordion.Item>
+                        <Accordion.Item value="item-2">
+                            <Accordion.Header>
+                                <Accordion.Trigger className="AccordionTrigger">
+                                    <span>Trigger text</span>
+                                    <ChevronDownIcon className="AccordionChevron" aria-hidden />
+                                </Accordion.Trigger>
+                            </Accordion.Header>
+                            <Accordion.Content>AA</Accordion.Content>
+                            <Accordion.Content>AA</Accordion.Content>
+                        </Accordion.Item>
+                    </Accordion.Root>
+                    <Flex id="menu-more-options"></Flex>
+                </Flex>
+            </Theme>
         </>
     )
 }
