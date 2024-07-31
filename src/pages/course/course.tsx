b6f4b474cfcaa91bc5e358c6354229476aa9f39f
André Rodrigues <andre.rodrigues0018@gmail.com>
2024-07-30 22:38:32 -0300

    feat: created course page

---
+++ src/pages/course/course.tsx
@@ -0,0 +1,14 @@
+import { Theme, Flex, Text, Button } from '@radix-ui/themes';
+// import './course.css'
+import codeschool from '../../assets/codeschool.svg';
+import { CheckIcon, RocketIcon, MixIcon } from '@radix-ui/react-icons';
+
+function Course() {
+    return (
+        <>
+            <p>cursos</p>
+        </>
+    )
+}
+
+export default Course
