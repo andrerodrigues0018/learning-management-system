e9d125c5b00d6e455955a3ad45e46c47ac1d797a
André Rodrigues <andre.rodrigues0018@gmail.com>
2024-08-17 14:26:13 -0300

    feat: menu implemented

--- src/pages/course/course.tsx
+++ src/pages/course/course.tsx
@@ -1,51 +1,77 @@
-import { Theme, Flex, Text, Button } from '@radix-ui/themes';
+import { Theme, Flex } from '@radix-ui/themes';
 import * as Accordion from '@radix-ui/react-accordion';
 import './course.css'
 import codeschool from '../../assets/codeschool.svg';
-import { CheckIcon, RocketIcon, MixIcon, ChevronDownIcon } from '@radix-ui/react-icons';
+import { ChevronDownIcon, DotFilledIcon, HomeIcon,  Crosshair2Icon, CardStackMinusIcon, PersonIcon, MixerVerticalIcon, IdCardIcon } from '@radix-ui/react-icons';
 
 
 function Course() {
     return (
         <>
             <Theme accentColor="blue" appearance='dark' grayColor="sand" radius="large">
+                <Flex id='body'>
+                    <Flex id='side-menu' direction={'column'} >
+                        <img id="menu-logo" src={codeschool}/>
+                        <Accordion.Root type="single" aria-expanded="false" collapsible={false} id='menu-items'>
+                            <Accordion.Item value="menu-item-dashboard" className="AccordionItem">
+                                <Accordion.Header>
+                                    <Accordion.Trigger className="AccordionTrigger">
+                                        <HomeIcon className="header-icon" aria-hidden />
 
-                <Flex id='side-menu' direction={'column'} >
-                    <img id="menu-logo" src='/vite.svg'/>
-                    <Accordion.Root type="multiple" aria-expanded="false" id='menu-items'>
-                        <Accordion.Item value="menu-item-dashboard" className="AccordionItem">
-                            <Accordion.Header>
-                                <RocketIcon className="header-icon" aria-hidden />
+                                        <span>Dashboard</span>
+                                    </Accordion.Trigger>
+                                </Accordion.Header>
+                            </Accordion.Item>
+                            <Accordion.Item value="menu-item-courses" className="AccordionItem">
+                                <Accordion.Header>
+                                    <Accordion.Trigger className="AccordionTrigger">
+                                        <CardStackMinusIcon className="header-icon" aria-hidden />
+                                        <span>My Courses</span>
+                                        <ChevronDownIcon className="AccordionChevron" aria-hidden />
+                                    </Accordion.Trigger>
+                                </Accordion.Header>
+                                <Accordion.Content>
+                                    <Flex className='item-icon'>
+                                        <DotFilledIcon/>
+                                        <span>Python</span>
+                                    </Flex>
+                                    <Flex className='item-icon'>
+                                        <DotFilledIcon color='lightblue'/>
+                                        <span>Python</span>
+                                    </Flex>
+                                    
+                                </Accordion.Content>
+                            </Accordion.Item>
+                            <Accordion.Item value="menu-item-skill" className="AccordionItem">
+                                <Accordion.Header>
+                                    <Accordion.Trigger className="AccordionTrigger">
+                                        <Crosshair2Icon className="header-icon" aria-hidden />
+                                        <span>Skill Tests</span>
+                                    </Accordion.Trigger>
+                                </Accordion.Header>
+                            </Accordion.Item>
+                            <Accordion.Item value="menu-item-certificates" className="AccordionItem">
+                                <Accordion.Header>
+                                    <Accordion.Trigger className="AccordionTrigger">
+                                        <IdCardIcon className="header-icon" aria-hidden />
+                                        <span>Certificates</span>
+                                    </Accordion.Trigger>
+                                </Accordion.Header>
+                            </Accordion.Item>
+                        </Accordion.Root>
+                        <Flex id="menu-more-options" direction={'column'}>
+                            <Flex className='more-option'>
+                                <MixerVerticalIcon/>
+                                <span>Settings</span>
+                            </Flex>
+                            <Flex className='more-option'>
+                                <PersonIcon/>
+                                <span>Account</span>
+                            </Flex>
+                        </Flex>
+                    </Flex>
+                    <Flex id="content">
 
-                                <Accordion.Trigger className="AccordionTrigger">
-                                    <span>Dashboard</span>
-                                </Accordion.Trigger>
-                            </Accordion.Header>
-                        </Accordion.Item>
-                        <Accordion.Item value="menu-item-certificates" className="AccordionItem">
-                            <Accordion.Header>
-                                <span>Certificates</span>
-                            </Accordion.Header>
-                        </Accordion.Item>
-                        <Accordion.Item value="menu-item-courses" className="AccordionItem">
-                            <Accordion.Header>
-                                <Accordion.Trigger className="AccordionTrigger">
-                                    <span>My Courses</span>
-                                    <ChevronDownIcon className="AccordionChevron" aria-hidden />
-                                </Accordion.Trigger>
-                            </Accordion.Header>
-                            <Accordion.Content>Python</Accordion.Content>
-                        </Accordion.Item>
-                    </Accordion.Root>
-                    <Flex id="menu-more-options" direction={'column'}>
-                        <Flex className='more-option' align={'center'}>
-                            <MixIcon/>
-                            <p>Settings</p>
-                        </Flex>
-                        <Flex className='more-option' align={'center'}>
-                            <MixIcon/>
-                            <p>Settings</p>
-                        </Flex>
                     </Flex>
                 </Flex>
             </Theme>
