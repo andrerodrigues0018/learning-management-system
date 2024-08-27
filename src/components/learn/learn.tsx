69d53b0d73b54ef8336524c18611a210345ede20
André Rodrigues <andre.rodrigues0018@gmail.com>
2024-08-26 23:04:23 -0300

    fix: course header

--- src/components/learn/learn.tsx
+++ src/components/learn/learn.tsx
@@ -1,8 +1,26 @@
+import { Flex } from "@radix-ui/themes";
 
 
 export function Learn() {
     return (
-        <>Learn</>
+        <>
+            <Flex>
+                <Flex id="learn-header">
+                    <Flex id="header-right">
+                        <i></i>
+                        <img/>
+                        <h2>Course Name</h2>
+                    </Flex>
+                    <Flex>
+                        <i></i>
+                        <i></i>
+                    </Flex>
+                </Flex>
+                {/* <Flex id="course-options">
+                    <div 
+                </Flex> */}
+            </Flex>
+        </>
     )
 
 }
